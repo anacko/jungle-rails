16db2cce72c6d3bed9c17bfa496bfffce70ac498
Ana Carolina K <anacko@gmail.com>
2021-11-11 11:10:11 -0500

    add dashboard with categories and products info

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -3,5 +3,17 @@ class Admin::DashboardController < ApplicationController
   http_basic_authenticate_with name:ENV['BASIC_AUTH_USERNAME'], password:ENV['BASIC_AUTH_PASSWORD']
 
   def show
+    @total_count = Product.count
+    @categories_products = pair_category_products 
   end
+  
+  private
+  def pair_category_products
+    category_count = Product.group("category_id").count
+    category_info = []
+    category_count.each {|key, value| category_info << {"id": key, "name": Category.find(key).name, "number_of_products": value}}
+    category_info
+  end
+
+
 end
