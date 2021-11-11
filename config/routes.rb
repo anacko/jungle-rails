4e02db1a0171560eebe19b9b43be0c30d1dd8e38
Ana Carolina K <anacko@gmail.com>
2021-11-11 17:47:24 -0500

    add login and register

--- config/routes.rb
+++ config/routes.rb
@@ -20,7 +20,8 @@ Rails.application.routes.draw do
     resources :categories, only: [:index, :new, :create]
   end
 
-  resources :user, only: [:show, :new, :create]
+  resources :users, only: [:show, :new, :create]
+  resources :sessions, only: [:new, :create, :destroy]
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
