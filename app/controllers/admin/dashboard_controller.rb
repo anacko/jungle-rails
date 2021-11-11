5e820d5b0a1a07cb1df3d9965ef73ad8d1055a7c
Ana Carolina K <anacko@gmail.com>
2021-11-11 14:04:41 -0500

    replace list of category-products with counts for categories and products with links within admin space

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -4,16 +4,7 @@ class Admin::DashboardController < ApplicationController
 
   def show
     @total_count = Product.count
-    @categories_products = pair_category_products 
+    @total_categories = Category.count
   end
-  
-  private
-  def pair_category_products
-    category_count = Product.group("category_id").count
-    category_info = []
-    category_count.each {|key, value| category_info << {"id": key, "name": Category.find(key).name, "number_of_products": value}}
-    category_info
-  end
-
 
 end
