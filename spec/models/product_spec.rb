6f16a9c8aae3930f8cec736a790c09da77970599
Ana Carolina K <anacko@gmail.com>
2021-11-15 15:21:06 -0500

    add prod tests

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -10,10 +10,54 @@ RSpec.describe Product, type: :model do
         price: 10,
         quantity: 1000,
         category: @category
-
       )
       @product.save!
       expect(@product.id).to be_present
+    end
+
+    it 'should not record products with missing name' do
+      @category = Category.new(name: "Utilities")
+      @product = Product.new(
+        name: nil,
+        price: 10,
+        quantity: 1000,
+        category: @category
+      )
+      expect { @product.save! }.to raise_error { |error| @product.errors.full_messages.include?(error) }
+
+    end
+
+    it 'should not record products with missing price' do
+      @category = Category.new(name: "Utilities")
+      @product = Product.new(
+        name: "Reusable Auto-sealing Envelope",
+        #price: nil,
+        quantity: 1000,
+        category: @category
+      )
+      expect { @product.save! }.to raise_error { |error| @product.errors.full_messages.include?(error) }
+    end
+
+    it 'should not record products with missing quantity' do
+      @category = Category.new(name: "Utilities")
+      @product = Product.new(
+        name: "Reusable Auto-sealing Envelope",
+        price: 10,
+        quantity: nil,
+        category: @category
+      )
+      expect { @product.save! }.to raise_error { |error| @product.errors.full_messages.include?(error) }
+    end
+
+    it 'should not record products with missing category' do
+      @category = Category.new(name: "Utilities")
+      @product = Product.new(
+        name: "Reusable Auto-sealing Envelope",
+        price: 10,
+        quantity: 1000,
+        category: nil
+      )
+      expect { @product.save! }.to raise_error { |error| @product.errors.full_messages.include?(error) }
     end
 
 
