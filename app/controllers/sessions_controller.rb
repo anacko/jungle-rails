417a36984f98cad32c8bb73da813f78ac529d7e4
Ana Carolina K <anacko@gmail.com>
2021-11-15 17:27:08 -0500

    fix login bug

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,9 +3,9 @@ class SessionsController < ApplicationController
     @user = User.new
   end
 
-  def create
-    if user = User.authenticate_with_credentials(params[:email], params[:password])
-      session[:user_id] = user.id
+  def create 
+    if @user = User.authenticate_with_credentials(params[:email], params[:password])
+      session[:user_id] = @user.id
       redirect_to root_path, notice: 'Welcome!'
     else
       render :new, notice: 'Login failed.'
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ActiveRecord::Base
   validates :email, uniqueness: { case_sensitive: false }
   validates :password, length: { minimum: 6 }
 
-  def authenticate_with_credentials (email, password)
+  def self.authenticate_with_credentials(email, password)
     @user = User.find_by_email(email.strip.downcase)
     if @user
       @user = @user.authenticate(password) if @user
