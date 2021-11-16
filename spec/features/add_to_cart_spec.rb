997d07dccdbe8e5fb3c03ac5a1532a167f3b5e74
Ana Carolina K <anacko@gmail.com>
2021-11-16 16:01:18 -0500

    add test when user adds an item to cart

---
+++ spec/features/add_to_cart_spec.rb
@@ -0,0 +1,35 @@
+require 'rails_helper'
+
+RSpec.feature "Visitor navigates to product page", type: :feature, js: true do
+
+  # SETUP
+  before :each do
+    @category = Category.create! name: 'Apparel'
+
+    1.times do |n|
+      @category.products.create!(
+        name:  Faker::Hipster.sentence(3),
+        description: Faker::Hipster.paragraph(4),
+        image: open_asset('apparel1.jpg'),
+        quantity: 10,
+        price: 64.99
+      )
+    end
+  end
+
+  scenario "They see the product page when click on product" do
+    # ACT
+    visit root_path
+    
+    page
+      .find('article.product', match: :first)
+      .click_on('Add')
+    sleep(1)
+
+    # DEBUG
+    # save_and_open_screenshot
+
+    # VERIFY
+    expect(page).to have_content 'My Cart (1)'
+  end
+end
